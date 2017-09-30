4833e21e472b30c7d4120b3140148a76abc33d79
bqcuong <bqcuong2212@gmail.com>
2017-09-30 21:50:09 +0700

    save db to file

--- uetscholarship-messenger/src/main/java/net/bqc/uetscholarship/messenger/config/DataSourceConfiguration.java
+++ uetscholarship-messenger/src/main/java/net/bqc/uetscholarship/messenger/config/DataSourceConfiguration.java
@@ -2,23 +2,41 @@ package net.bqc.uetscholarship.messenger.config;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.Resource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 
 @Configuration
+@PropertySource("classpath:persistence.properties")
 public class DataSourceConfiguration {
 
+	@Autowired
+	private Environment env;
+	
+	@Value("classpath:db/create-db.sql")
+	public Resource schemaScript;
+	
 	@Bean
 	public DataSource dataSource() {
-		EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
-		EmbeddedDatabase db = builder
-				.setType(EmbeddedDatabaseType.HSQL)
-				.addScript("db/create-db.sql")
-				.addScript("db/init-db.sql")
-				.build();
-		return db;
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(env.getProperty("jdbc.driver"));
+		dataSource.setUrl(env.getProperty("jdbc.url"));
+		dataSource.setUsername(env.getProperty("jdbc.username"));
+		dataSource.setPassword(env.getProperty("jdbc.password"));
+		
+		// add script
+		ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+		populator.addScript(schemaScript);
+		DatabasePopulatorUtils.execute(populator, dataSource);
+		
+		return dataSource;
 	}
 }
