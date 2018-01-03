18b7325cd8a35b4fc95b671f51b3a102d35024cb
bqcuong <bqcuong2212@gmail.com>
2018-01-03 21:37:02 +0700

    update new module: uet grade server

--- uetgrade-server/src/main/java/net/bqc/uss/uetgrade_server/UetGradeApplication.java
+++ uetgrade-server/src/main/java/net/bqc/uss/uetgrade_server/UetGradeApplication.java
@@ -1,4 +1,14 @@
 package net.bqc.uss.uetgrade_server;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
 public class UetGradeApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(UetGradeApplication.class).web(false).run(args);
+    }
 }
