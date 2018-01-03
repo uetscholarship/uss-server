391fc2ab4b9cc6faaa32961d36bd34fbc436bd8f
bqcuong <bqcuong2212@gmail.com>
2018-01-03 21:32:46 +0700

    run uetnews server without being in webapp-based model

--- uetnews-server/src/main/java/net/bqc/uss/uetnews_server/UetNewsApplication.java
+++ uetnews-server/src/main/java/net/bqc/uss/uetnews_server/UetNewsApplication.java
@@ -2,6 +2,7 @@ package net.bqc.uss.uetnews_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
@@ -9,7 +10,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class UetNewsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UetNewsApplication.class, args);
+//		SpringApplication.run(UetNewsApplication.class, args);
+		new SpringApplicationBuilder(UetNewsApplication.class).web(false).run(args);
 	}
 	
 }
