64d716b0afd46e95c263c3475996456cf3de7d7b
bqcuong <bqcuong2212@gmail.com>
2018-01-07 21:10:29 +0700

    fix unexpected bug of uetnews-server

--- uetnews-server/src/main/java/net/bqc/uss/uetnews_server/UetNewsApplication.java
+++ uetnews-server/src/main/java/net/bqc/uss/uetnews_server/UetNewsApplication.java
@@ -1,16 +1,23 @@
 package net.bqc.uss.uetnews_server;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {
+		DataSourceAutoConfiguration.class,
+		DataSourceTransactionManagerAutoConfiguration.class,
+		HibernateJpaAutoConfiguration.class
+})
 @EnableScheduling
 public class UetNewsApplication {
 
 	public static void main(String[] args) {
-//		SpringApplication.run(UetNewsApplication.class, args);
 		new SpringApplicationBuilder(UetNewsApplication.class).web(false).run(args);
 	}
 	
