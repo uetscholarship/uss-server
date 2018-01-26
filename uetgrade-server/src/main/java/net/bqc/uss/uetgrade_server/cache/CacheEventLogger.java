e6ccdf174c4a49293ade0106ada33310f0ea1671
bqcuong <bqcuong2212@gmail.com>
2018-01-26 16:33:19 +0700

    useing cache alias instead jcache customizer

--- uetgrade-server/src/main/java/net/bqc/uss/uetgrade_server/cache/CacheEventLogger.java
+++ uetgrade-server/src/main/java/net/bqc/uss/uetgrade_server/cache/CacheEventLogger.java
@@ -5,9 +5,9 @@ import org.ehcache.event.CacheEventListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class GradeCacheEventLogger implements CacheEventListener<Object, Object> {
+public class CacheEventLogger implements CacheEventListener<Object, Object> {
 
-    public static final Logger logger = LoggerFactory.getLogger(GradeCacheEventLogger.class);
+    public static final Logger logger = LoggerFactory.getLogger(CacheEventLogger.class);
 
     @Override
     public void onEvent(CacheEvent<?, ?> cacheEvent) {
